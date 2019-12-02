ef34c1bae06b65eb886fb3e6fae9960cc3afc3ae
vevev <taibaihat.mobi@gmail.com>
2019-12-02 15:08:26 +0700

    sss

--- app/Acme/Services/Fetchs/FetchSkymusicDatabase.php
+++ app/Acme/Services/Fetchs/FetchSkymusicDatabase.php
@@ -27,14 +27,10 @@ class FetchSkymusicDatabase
     public function execute(int $page = 1)
     {
         try {
-            $url_format = 'http://api.skysoundtrack.vn/song/search?token=eyJhbGciOiJIUzI1NiJ9.eyJ1aWQiOiIyNjcxIiwibmJmIjoxNTczMjA3Mzc3LCJleHAiOjE2MDQ3NDMzNzcsImlhdCI6MTU3MzIwNzM3NywiZGlkIjoiU0tZTVVTSUNBOTFFN0JGQUVGQjY3M0M3MkVGMzJFQTUiLCJqdGkiOiIyMDU2YzNjODdhMzI0ODE5ODRlYTlkNmMyYWI1YjE4NiJ9.R6rxBV-tzOSkpUusPo38VCqOYFiT2SkOJhboMqcx-fA&client_id=9&timestamp=1573442753256&checksum=4039f084bd8637e2f750d906b0670af4e50524fe04633b89ab40a44c440afd1e&page_size=50&page_index=%d';
+            $url_format = 'http://api.skysoundtrack.vn/song/search?token=eyJhbGciOiJIUzI1NiJ9.eyJ1aWQiOiIyNjcxIiwibmJmIjoxNTczMjA3Mzc3LCJleHAiOjE2MDQ3NDMzNzcsImlhdCI6MTU3MzIwNzM3NywiZGlkIjoiU0tZTVVTSUNBOTFFN0JGQUVGQjY3M0M3MkVGMzJFQTUiLCJqdGkiOiIyMDU2YzNjODdhMzI0ODE5ODRlYTlkNmMyYWI1YjE4NiJ9.R6rxBV-tzOSkpUusPo38VCqOYFiT2SkOJhboMqcx-fA&client_id=9&timestamp=1573442753256&checksum=4039f084bd8637e2f750d906b0670af4e50524fe04633b89ab40a44c440afd1e&page_size=30&page_index=%d';
             $url        = sprintf($url_format, $page < 1 ? 1 : $page);
             $jsonString = $this->httpRequest->get($url);
             $json       = json_decode($jsonString);
-            if ( ! isset($json->data)) {
-                echo $url;
-                dd($json);
-            }
 
             return $json->data ?? $json->error;
         } catch (Throwable $e) {
