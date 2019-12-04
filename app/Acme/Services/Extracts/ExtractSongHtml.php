4477f795ba9c9eb768ca2b3730e4ff0933caefaf
vevev <taibaihat.mobi@gmail.com>
2019-12-04 10:46:57 +0700

    asdADS

--- app/Acme/Services/Extracts/ExtractSongHtml.php
+++ app/Acme/Services/Extracts/ExtractSongHtml.php
@@ -32,7 +32,6 @@ class ExtractSongHtml
         if ( ! preg_match_all($patternSingle, $ul[0], $matchesSingle, PREG_SET_ORDER)) {
             return;
         }
-        exit($html);
         $patternKey = '#(?=<span keyEncrypt="([^"]+?)")#is';
         if ( ! preg_match_all($patternKey, $ul[0], $matchesKey, PREG_SET_ORDER)) {
             return;
@@ -42,6 +41,7 @@ class ExtractSongHtml
         if ( ! preg_match_all($patternSrc, $ul[0], $matchesSrc, PREG_SET_ORDER)) {
             return;
         }
+        dd($matchesSrc, $matchesKey);
 
         $patternRealId = '#(?=song_img_(\d+))#i';
         if ( ! preg_match_all($patternRealId, $ul[0], $matchesRealId, PREG_SET_ORDER)) {
