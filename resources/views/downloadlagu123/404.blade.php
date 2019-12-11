dc8a9f2399265f5e0da333e75ed8f0c426ec5299
vevev <taibaihat.mobi@gmail.com>
2019-12-11 12:03:47 +0700

    www

--- resources/views/downloadlagu123/404.blade.php
+++ resources/views/downloadlagu123/404.blade.php
@@ -1,6 +1,28 @@
-<div class="main_search" style="padding: 10px">
-    <p>
-        <center><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Không tìm thấy nội dung</font></font><p><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
-       Nội dung này không tồn tại. </font></font><a href="/"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Nhấn vào đây</font></font></font></font></a><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> để trở về trang chủ</font></font></p></center>
-    </p>
-</div>
+<html lang="id"><head>
+<title>Không tìm thấy nội dung</title>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+    <body>
+        <center>Không tìm thấy nội dung<p>
+        Nội dung này không tồn tại. <a href="/">Nhấn vào đây</a>để trở về trang chủ
+        </center>
+
+    <style>
+    center{
+        background: rgba(204, 204, 204, 0.15);
+        color: #111;
+        padding: 20px 10px;
+        border: 1px solid rgba(0, 0, 0, 0.2);
+        box-shadow: 0 0 6px 1px rgba(0, 0, 0, 0.08);
+        font-weight: bold;
+        font-family: monospace;
+        font-size: 20px;
+    }
+    center>p{
+        font-size: 13px;
+
+    }
+    </style>
+</body><style id="igor_ext_nofollow">a[rel~='nofollow']{text-decoration: underline;
+border-bottom: 1px dashed red;}a[rel~='nofollow'] > img{outline:thin dotted red !important; outline-offset:.2em;}</style></html>
