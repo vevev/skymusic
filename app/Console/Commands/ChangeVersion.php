5cc555328487cf3cc2b311f37b9c9f40625d5238
vevev <taibaihat.mobi@gmail.com>
2019-12-13 10:52:32 +0700

    hello

--- app/Console/Commands/ChangeVersion.php
+++ app/Console/Commands/ChangeVersion.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use Throwable;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
 
 class ChangeVersion extends Command
 {
@@ -37,6 +39,20 @@ class ChangeVersion extends Command
      */
     public function handle()
     {
-        //
+        try {
+            $env_string = file_get_contents(base_path() . '/.env', true);
+            $env_string = preg_replace_callback(
+                '#(APP_STATIC_VERSION=.+\.)(\d+)#',
+                function ($matches) {
+                    return $matches[1] . (++$matches[2]);
+                }, $env_string);
+
+            file_put_contents(base_path() . '/.env', $env_string);
+
+            Artisan::call('config:cache');
+            $this->line('<fg=green>Version Changed !</>');
+        } catch (Throwable $e) {
+            $this->line('<fg=red>Error !</>');
+        }
     }
 }
