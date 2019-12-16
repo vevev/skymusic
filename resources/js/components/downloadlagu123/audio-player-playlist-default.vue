c29efabb6ed8baa41b11b6a9bfc29b5018405b24
vevev <taibaihat.mobi@gmail.com>
2019-12-16 22:34:44 +0700

    fix

--- resources/js/components/downloadlagu123/audio-player-playlist-default.vue
+++ resources/js/components/downloadlagu123/audio-player-playlist-default.vue
@@ -13,22 +13,24 @@
             Trình duyệt của bạn không hỗ trợ nghe online !
         </audio>
         <div id="songs-container">
-            <div
-                v-for="(song, i) in songs"
-                :class="{ song: true, songplay: song.play }"
-                @click.stop="onCLickMenu(i)"
-            >
+            <div v-for="(song, i) in songs" :class="{ song: true, songplay: i == index }">
                 <div class="status">
                     <div class="playing icon-play" v-if="!song.play"></div>
                     <div class="pause icon-pause" v-if="song.play"></div>
                 </div>
                 <div class="info">
                     <h3 class="name">
-                        <a :href="song.detail_url" :title="title(song.name)">
+                        <a
+                            :href="song.detail_url"
+                            :title="title(song.name)"
+                            @click.stop.prevent="onCLickMenu(i)"
+                        >
                             {{ song.name }}
                         </a>
                     </h3>
                     <span class="sg">
+                        <b class="download"><a :href="streamUrl(i)">Tải nhạc</a></b>
+
                         <b class="single">{{ song.single }}</b>
                     </span>
                 </div>
@@ -63,6 +65,10 @@ export default {
             this.index = index;
         },
 
+        onClickDownload(index) {
+            console.log(index);
+        },
+
         onPlay() {
             if (this.index === -1) {
                 return (this.index = 0);
@@ -76,16 +82,21 @@ export default {
 
         onEnded() {
             if (!this.songs[++this.index]) this.index = 0;
-            this.$refs.audio.src =
-                '/listen/' +
-                this.songs[this.index].slug +
-                '.' +
-                this.songs[this.index].song_id +
-                '.' +
-                this.playlistid +
-                '.html';
+            this.$refs.audio.src = this.streamUrl(this.index);
             this.$refs.audio.load();
             this.$refs.audio.play();
+        },
+
+        streamUrl(index) {
+            return (
+                '/listen/' +
+                this.songs[index].slug +
+                '.' +
+                this.songs[index].song_id +
+                '.' +
+                this.playlistid +
+                '.html'
+            );
         },
 
         async onError() {
@@ -105,14 +116,7 @@ export default {
         index(n, o) {
             if (n != o) {
                 o != -1 && this.$set(this.songs[o], 'play', false);
-                this.$refs.audio.src =
-                    '/listen/' +
-                    this.songs[n].slug +
-                    '.' +
-                    this.songs[n].song_id +
-                    '.' +
-                    this.playlistid +
-                    '.html';
+                this.$refs.audio.src = this.streamUrl(n);
                 this.$refs.audio.load();
                 this.$refs.audio.play();
             }
@@ -131,7 +135,7 @@ export default {
     position: relative;
     height: 100%;
     padding: 10px 0;
-    margin: 0 10px;
+    margin: 0px;
     overflow: hidden;
     border: none;
     border-bottom: 1px solid #ededed;
@@ -139,8 +143,7 @@ export default {
 
     .status {
         height: 40px;
-        width: 40px;
-        float: left;
+        width: 30px;
         display: flex;
         flex-shrink: 0;
         .pause,
@@ -154,7 +157,7 @@ export default {
                 align-items: center;
                 margin: 0;
                 display: flex;
-                font-size: 25px;
+                font-size: 15px;
                 color: #888;
             }
         }
@@ -167,6 +170,8 @@ export default {
         display: block;
         padding-bottom: 0;
         border: none;
+        min-width: 0;
+        flex-grow: 1;
         h3.name {
             font-size: 15px;
             display: block;
@@ -174,21 +179,56 @@ export default {
             overflow: hidden;
             text-overflow: ellipsis;
         }
+        .sg {
+            display: flex;
+            margin-top: 5px;
+            b {
+                display: flex;
+                align-items: center;
+                padding: 0px 5px;
+                a {
+                    display: flex;
+                    align-items: center;
+                    font-size: inherit;
+                }
+            }
+            .download {
+                font-size: 12px !important;
+                background: #ffffff00;
+                margin-right: 10px;
+                color: inherit;
+                cursor: pointer;
+                &:before {
+                    content: '\a021';
+                    font-family: fonts;
+                    padding-right: 5px;
+                }
+                &:hover {
+                    background: #805b00;
+                    a {
+                        color: #fff;
+                    }
+                    &:before {
+                        color: #fff;
+                    }
+                }
+            }
+        }
     }
     &:after {
         content: '';
         display: block;
         clear: both;
     }
-    &:before {
-        content: '';
-        display: flex;
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        top: 0;
-        left: 0;
-    }
+    // &:before {
+    //     content: '';
+    //     display: flex;
+    //     position: absolute;
+    //     width: 100%;
+    //     height: 100%;
+    //     top: 0;
+    //     left: 0;
+    // }
 }
 .songplay {
     background: #ededed;
