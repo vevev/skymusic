bb6a774bc825885bd9c8c84819f607d496ed56d6
vevev <taibaihat.mobi@gmail.com>
2019-12-25 09:51:07 +0700

    ssss

--- app/Acme/Services/Adapters/LoadIndexData.php
+++ app/Acme/Services/Adapters/LoadIndexData.php
@@ -19,16 +19,15 @@ class LoadIndexData
 
     public function execute()
     {
-        $main     = $this->loadTop20Song->execute('vn', 'bai-hat');
-        $playlist = $this->getPlaylist->execute('iZ7XNosoPhJN');
-        $playlist->load('songs');
+        $main           = $this->loadTop20Song->execute('vn', 'bai-hat');
+        $playlist_songs = $this->getPlaylist->execute('iZ7XNosoPhJN')->songs;
 
         return [
             'main'    => $main,
             'sidebar' => [
                 //'primary'   => $this->loadTop20Song->execute('us', 'bai-hat'),
                 //'secondary' => $this->loadTop20Song->execute('kr', 'bai-hat'),
-                'playlist' => $playlist,
+                'playlist' => $playlist_songs,
             ],
         ];
     }
--- resources/views/downloadlagu123/index.blade.php
+++ resources/views/downloadlagu123/index.blade.php
@@ -32,34 +32,6 @@
 @endsection
 
 @section('sidebar')
-{{--     <div class="right-bar">
-        <div class="cate">
-            <b class="ht">Tải Nhạc Mp3 Hay khác</b>
-        </div>
-        <div class="collapse-menu">
-        @foreach($data['sidebar']['primary'] as $song)
-        <div class="menu">
-            <div class="detail-thumb thumb">
-                <a title="Tải bài hát {{ $song['name'] }} - TaiNhac123" href="{{ route('song', ['slug'=>$song['slug'], 'id'=>$song['song_id']]) }}">
-                    <img alt="Tải bài hát {{ $song['name'] }} - TaiNhac123" title="Tải bài hát {{ $song['name'] }} - TaiNhac123" data-src="{{ $song['thumbnail'] }}" src="{{ asset('/images/audio_default.png') }}" />
-                </a>
-            </div>
-            <div class="detail-info">
-                <h3 class="ab ellipsis dli">
-                    <a title="Tải bài hát {{ $song['name'] }} - TaiNhac123" href="{{ route('song', ['slug'=>$song['slug'], 'id'=>$song['song_id']]) }}">{{ $song['name'] }}</a>
-                </h3>
-                <span class="sg">
-                    <b class="single">{{ $song['single'] }}</b>
-                    @if($song['listen'])
-                    <b class="play-count">{{ $song['listen'] }}</b>
-                    @endif
-                </span>
-            </div>
-        </div>
-        @endforeach
-        <p class="collapse-view-more">Xem Thêm ...</p>
-        </div>
-    </div> --}}
     <div class="right-bar">
         <div class="cate">
             <b class="ht">Nhạc Mp3 Hay khác</b>
