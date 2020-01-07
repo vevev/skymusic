d4b2c8f6d4a8e7616299c31b0a9cc65eb01a9f9a
vevev <taibaihat.mobi@gmail.com>
2020-01-07 16:52:07 +0700

    jkjhfgsjdfg

--- resources/views/downloadlagu123/includes/footers/footer.blade.php
+++ resources/views/downloadlagu123/includes/footers/footer.blade.php
@@ -8,24 +8,22 @@
 @endif
 
 <div id="footer">
-    <div class="center"><h3 class="f15">Trang web tải bài hát mới nhất, Download nhạc - kho bài hát mp3 miễn phí</h3></div>
-
+    <div class="center"><h3 class="f15">Trang web tải bài hát hay mới nhất. Download nhạc mp3 - kho bài hát miễn phí</h3></div>
 <p align="center">
 </p><br />
-<p align="center"><b><a href="/">TaiNhac123.Com</a></b> là trang web <i><u>Tải nhạc Mp3 Miễn Phí</u></i>, tải nhạc mp3 chất lượng cao, trang web cập nhật bảng xếp hạng âm nhạc chính xác và nhanh nhất,  kho bài hát lớn nhất có thể đáp ứng tất cả các nhu cầu của bạn, là lựa chọn hàng đầu cho bạn.
+<p align="center"><b><a href="/">TaiNhac123.Com</a></b> là trang web <i><u>Tải nhạc Mp3 Miễn Phí</u></i> về thẻ nhớ, tải nhạc mp3 chất lượng cao, trang web cập nhật bảng xếp hạng âm nhạc chính xác và nhanh nhất,  kho bài hát lớn nhất có thể đáp ứng tất cả các nhu cầu của bạn, là lựa chọn hàng đầu cho bạn.
 <br></p>
 <p class="center">
-
 <a href="/disclaimers.html" style="color:blue">Giới thiệu và Điều khoản</a>
 <br>
 <br>
   <a href="/" aria-label="Tải nhạc mp3 miễn phí"><img src="/images/icon-tainhac.png" width="50px" alt="Tải nhạc 123"></a><br>
-Blog <b><a href="/">TaiNhac123</a></b> <font size="4">✪</font><br>
+<b><a href="/">TaiNhac123</a></b> <font size="4">✪</font><br>
 @if(Page::$IS_ADSENSE)
 Bản quyền nội dung cung cấp bởi <b><a rel="nofollow" href="https://skymusic.com.vn/">SkyMusic</a></b>
+<br>
+<br>
 @endif
-<br>
-<br>
 Contact: ad.tainhac123@gmail.com<br>
 ♪ <b>FULL SONG FOR YOU</b> ♫<br>
 <font size="5">💖</font></p>
