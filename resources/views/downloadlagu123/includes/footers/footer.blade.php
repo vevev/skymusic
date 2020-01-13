66df18126efdf509347057fb1710ae5fe41fe959
vevev <taibaihat.mobi@gmail.com>
2020-01-13 08:14:18 +0700

    fix footer

--- resources/views/downloadlagu123/includes/footers/footer.blade.php
+++ resources/views/downloadlagu123/includes/footers/footer.blade.php
@@ -8,15 +8,17 @@
 @endif
 
 <div id="footer">
-    <div class="center"><h3 class="f15">Trang web tải bài hát hay mới nhất. Download nhạc mp3 - kho bài hát miễn phí</h3></div>
-<p align="center">
-</p><br />
-<p align="center"><b><a href="/">TaiNhac123.Com</a></b> là trang web <i><u>Tải nhạc Mp3 Miễn Phí</u></i> về thẻ nhớ cho máy Samsung, Oppo, Iphone ... Tải nhạc mp3 chất lượng cao, trang web cập nhật bảng xếp hạng âm nhạc chính xác và nhanh nhất,  kho bài hát lớn nhất có thể đáp ứng tất cả các nhu cầu của bạn, là lựa chọn hàng đầu cho bạn.
-<br></p>
+	@if (Page::$currentRouteName === 'index')
+    <div class="center">
+    	<h3 class="f15">Trang web tải bài hát hay mới nhất. Download nhạc mp3 - kho bài hát miễn phí</h3>
+    </div>
+	<br>
+	<p align="center">
+		<b><a href="/">TaiNhac123.Com</a></b> là trang web <i><u>Tải nhạc Mp3 Miễn Phí</u></i> về thẻ nhớ cho máy Samsung, Oppo, Iphone ... Tải nhạc mp3 chất lượng cao, trang web cập nhật bảng xếp hạng âm nhạc chính xác và nhanh nhất,  kho bài hát lớn nhất có thể đáp ứng tất cả các nhu cầu của bạn, là lựa chọn hàng đầu cho bạn.
+	</p>
+	<br>
+	@endif
 <p class="center">
-<a href="/disclaimers.html" style="color:blue">Giới thiệu và Điều khoản</a>
-<br>
-<br>
   <a href="/" aria-label="Tải nhạc mp3 miễn phí"><img src="/images/icon-tainhac.png" width="50px" alt="Tải nhạc 123"></a><br>
 <b><a href="/">TaiNhac123</a></b> <font size="4">✪</font><br>
 @if(Page::$IS_ADSENSE)
@@ -27,5 +29,5 @@ Bản quyền nội dung cung cấp bởi <b><a rel="nofollow" href="https://sky
 Contact: ad.tainhac123@gmail.com<br>
 ♪ <b>FULL SONG FOR YOU</b> ♫<br>
 <font size="5">💖</font></p>
-
+<a href="/disclaimers.html" style="color:blue">Giới thiệu và Điều khoản</a>
 </div>
