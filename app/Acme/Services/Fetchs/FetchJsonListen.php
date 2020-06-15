3981c9ab13aff019d28a7db7e759ddf884651ab0
vevev <taibaihat.mobi@gmail.com>
2020-06-15 10:32:45 +0700

    hello

--- app/Acme/Services/Fetchs/FetchJsonListen.php
+++ app/Acme/Services/Fetchs/FetchJsonListen.php
@@ -24,7 +24,7 @@ class FetchJsonListen
             $url_format = 'https://www.nhaccuatui.com/interaction/https/v2/counter?listSongIds=%s';
             $url        = sprintf($url_format, implode(',', $real_ids));
             $json       = $this->httpRequest->get($url);
-            file_put_contents("/home/nginx/"+time(), $json);
+            //file_put_contents("/home/nginx/"+time(), $json);
 
             return json_decode($json)->data->songs;
         } catch (Throwable $e) {
