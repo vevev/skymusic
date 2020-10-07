db4df19aed0e6b3d13129838a188ca6224487b4c
vevev <taibaihat.mobi@gmail.com>
2020-10-07 12:01:23 +0700

    ádas

--- app/Acme/Services/Extracts/ExtractSongHtml.php
+++ app/Acme/Services/Extracts/ExtractSongHtml.php
@@ -27,19 +27,19 @@ class ExtractSongHtml
         }
 
         if ( ! preg_match('#<ul id="ulSongRecommend">.+?</ul>#is', $html, $ul)) {
-            throw new ExtractSongException('ul not found', ['document' => $html]);
+            throw new ExtractSongException(json_encode(['ul not found', ['document' => $html]]));
         }
 
         // Lọc ra các row bài hát, nếu không có thì trả lại null
         $patternName = '#(?=<h3.*href=".*/bai-hat/([^/]+?)\.([^/]+?).html">(.+?)</a></h3>)#';
         if ( ! preg_match_all($patternName, $ul[0], $matchesName, PREG_SET_ORDER)) {
-            throw new ExtractSongException('name not found', ['document' => $ul[0]]);
+            throw new ExtractSongException(json_encode(['name not found', ['document' => $ul[0]]]));
         }
 
         // Lọc ra các single bài hát, nếu không có thì trả lại null
         $patternSingle = '#<h4 class="singer_song">.+?</h4>#is';
         if ( ! preg_match_all($patternSingle, $ul[0], $matchesSingle, PREG_SET_ORDER)) {
-            throw new ExtractSongException('single not found', ['document' => $ul[0]]);
+            throw new ExtractSongException(json_encode(['single not found', ['document' => $ul[0]]]));
         }
 
         $patternKey = '#(?=<span keyEncrypt="([^"]+?)")#is';
@@ -47,16 +47,16 @@ class ExtractSongHtml
 
         $patternSrc = '#(?=data-src="([^"]+?)")#i';
         if ( ! preg_match_all($patternSrc, $ul[0], $matchesSrc, PREG_SET_ORDER)) {
-            throw new ExtractSongException('src not found', ['document' => $ul[0]]);
+            throw new ExtractSongException(json_encode(['src not found', ['document' => $ul[0]]]));
         }
 
         $patternRealId = '#(?=song_img_(\d+))#i';
         if ( ! preg_match_all($patternRealId, $ul[0], $matchesRealId, PREG_SET_ORDER)) {
-            throw new ExtractSongException('realid not found', ['document' => $ul[0]]);
+            throw new ExtractSongException(json_encode(['realid not found', ['document' => $ul[0]]]));
         }
 
         if (count($matchesSingle) !== count($matchesName)) {
-            throw new ExtractSongException('song array not equal', [$matchesSingle, $matchesName]);
+            throw new ExtractSongException(json_encode(['song array not equal', [$matchesSingle, $matchesName]]));
         }
 
         $songs = [];
