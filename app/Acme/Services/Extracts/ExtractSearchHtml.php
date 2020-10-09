61c508c09abbf47d412033710043083373ff18b2
vevev <taibaihat.mobi@gmail.com>
2020-10-09 10:56:36 +0700

    fix search

--- app/Acme/Services/Extracts/ExtractSearchHtml.php
+++ app/Acme/Services/Extracts/ExtractSearchHtml.php
@@ -11,57 +11,52 @@ class ExtractSearchHtml
 
     public function execute(string $html)
     {
-
-        // Lọc ra các row bài hát, nếu không có thì trả lại null
-        $patternName = '#(?=<h3.*href=".*/bai-hat/([^/]+?)\.([^/]+?).html">(.+?)</a></h3>)#';
-        if ( ! preg_match_all($patternName, $html, $matchesName, PREG_SET_ORDER)) {
+        $patternLi = '#<li.*?>.+?</li>#s';
+        if ( ! preg_match_all($patternLi, $html, $matchesLi)) {
             return;
         }
 
-        // Lọc ra các single bài hát, nếu không có thì trả lại null
-        $patternSingle = '#<h4 class="singer_song">.+?</h4>#is';
-        if ( ! preg_match_all($patternSingle, $html, $matchesSingle, PREG_SET_ORDER)) {
-            return;
-        }
+        $song = [];
+        foreach ($matchesLi[0] as $index => $li) {
+            // Lọc ra các row bài hát, nếu không có thì trả lại null
+            $patternName = '#(?=<h3.*href=".*/bai-hat/([^/]+?)\.([^/]+?).html">(.+?)</a></h3>)#';
+            if ( ! preg_match($patternName, $li, $matchesName)) {
+                return;
+            }
 
-        $patternKey = '#(?=<li class="song_item_single">.+?lp="([^"]+?)".+?<span keyEncrypt="([^"]+?)")#is';
-        if ( ! preg_match_all($patternKey, $html, $matchesKey, PREG_SET_ORDER)) {
-            return;
-        }
+            // Lọc ra các single bài hát, nếu không có thì trả lại null
+            $patternSingle = '#<h4 class="singer_song">.+?</h4>#is';
+            if ( ! preg_match($patternSingle, $li, $matchesSingle)) {
+                return;
+            }
 
-        $patternSrc = '#(?=src="([^"]+?)")#i';
-        preg_match_all($patternSrc, $html, $matchesPrimarySrc, PREG_SET_ORDER);
-
-        $patternSrc = '#(?=data-src="([^"]*?)")#i';
-        if ( ! preg_match_all($patternSrc, $html, $matchesSrc, PREG_SET_ORDER)) {
-            return;
-        }
-
-        $patternRealId = '#(?=song_img_(\d+))#i';
-        if ( ! preg_match_all($patternRealId, $html, $matchesRealId, PREG_SET_ORDER)) {
-            return;
-        }
-
-        if (count($matchesSingle) !== count($matchesName)
-            || count($matchesSingle) !== count($matchesKey)) {
-            return;
-        }
-
-        $songs = [];
-        foreach ($matchesName as $index => $match) {
-            if ("false" == $matchesKey[$index][1]) {
+            $patternKey = '#(?=<span keyEncrypt="([^"]+?)")#is';
+            if ( ! preg_match($patternKey, $li, $matchesKey)) {
                 continue;
             }
 
-            preg_match_all('#(?=<a[^>]+?>(.+?)</a>)#', $matchesSingle[$index][0], $single);
-            $thumb   = $matchesSrc[$index][1] ? $matchesSrc[$index][1] : $matchesPrimarySrc[$index][1];
+            $patternSrc = '#(?=src="([^"]+?)")#i';
+            preg_match($patternSrc, $li, $matchesPrimarySrc);
+
+            $patternSrc = '#(?=data-src="([^"]*?)")#i';
+            if ( ! preg_match($patternSrc, $li, $matchesSrc)) {
+                return;
+            }
+
+            $patternRealId = '#(?=song_img_(\d+))#i';
+            if ( ! preg_match($patternRealId, $li, $matchesRealId)) {
+                return;
+            }
+
+            preg_match_all('#(?=<a[^>]+?>(.+?)</a>)#', $matchesSingle[0], $single);
+            $thumb   = $matchesSrc[1] ? $matchesSrc[1] : $matchesPrimarySrc[1];
             $songs[] = [
-                'slug'      => $match[1],
-                'real_id'   => $matchesRealId[$index][1],
+                'slug'      => $matchesName[1],
+                'real_id'   => $matchesRealId[1],
                 'thumbnail' => $thumb,
-                'song_id'   => $match[2],
-                'name'      => $match[3],
-                'key'       => $matchesKey[$index][2],
+                'song_id'   => $matchesName[2],
+                'name'      => $matchesName[3],
+                'key'       => $matchesKey[1],
                 'single'    => implode(',', $single[1]),
             ];
         }
