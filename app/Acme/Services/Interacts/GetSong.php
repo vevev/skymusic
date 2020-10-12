ef088360f0bc9f9529980e951077604626f3048b
vevev <taibaihat.mobi@gmail.com>
2020-10-12 12:10:28 +0700

    da

--- app/Acme/Services/Interacts/GetSong.php
+++ app/Acme/Services/Interacts/GetSong.php
@@ -4,23 +4,27 @@ namespace App\Acme\Services\Interacts;
 
 use App\Models\NCTSong;
 use Illuminate\Support\Facades\Cache;
+use App\Acme\Services\Interacts\CacheSong;
 
 class GetSong
 {
     private $song;
+    private $cacheSong;
     /**
      * [__construct description]
      *
-     * @param NCTSong $song [description]
+     * @param      NCTSong                                 $song       [description]
+     * @param      \App\Acme\Services\Interacts\CacheSong  $cacheSong  The cache song
      */
-    public function __construct(NCTSong $song)
+    public function __construct(NCTSong $song, CacheSong $cacheSong)
     {
-        $this->song = $song;
+        $this->song      = $song;
+        $this->cacheSong = $cacheSong;
     }
 
     public function execute(string $id)
     {
-        if ($song = Cache::store('redis')->get('song:' . $id)) {
+        if ($song = $this->cacheSong->get($id)) {
             $song->cached = true;
 
             return $song;
