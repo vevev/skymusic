050c1d43ea5e415416852e408db532962ece374a
vevev <taibaihat.mobi@gmail.com>
2021-03-12 11:32:46 +0700

    jlugljh

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -4,47 +4,43 @@ namespace App\Http\Controllers;
 
 use App\Acme\Core;
 use App\Acme\Page;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Config;
 use App\Acme\Services\Adapters\LoadSongData;
+use Illuminate\Http\Request;
 
-class SongController extends Controller
-{
-    private $loadSongData;
+class SongController extends Controller {
+	private $loadSongData;
 
-    public function __construct(LoadSongData $loadSongData)
-    {
-        $this->loadSongData = $loadSongData;
-    }
+	public function __construct(LoadSongData $loadSongData) {
+		$this->loadSongData = $loadSongData;
+	}
 
-    public function index(Request $request, Core $core)
-    {
-        if ($request->slug_unverify || $request->id_unverify) {
-            return view(Core::viewPath('song-slug'));
-        }
+	public function index(Request $request, Core $core) {
+		if ($request->slug_unverify || $request->id_unverify) {
+			return view(Core::viewPath('song-slug'));
+		}
 
-        if ("NyfHSqDdNWs3" == $request->id) {
-            Config::set('app.debug', true);
-        }
+		// if ("NyfHSqDdNWs3" == $request->id) {
+		// 	Config::set('app.debug', true);
+		// }
 
-        $data = $this->loadSongData->execute($request->id);
+		$data = $this->loadSongData->execute($request->id);
 
-        Page::$title       = 'Tải ' . $data['song']->name . ' MP3 - Download Miễn Phí';
-        Page::$description = $data['song']->name . ' Mp3, tải bài hát ' . $data['song']->name . ' - ' . $data['song']->single . ', tải nhạc chất lượng cao. Miễn phí download về máy. Tải dễ dàng và nhanh chóng ♥';
+		Page::$title = 'Tải Bài Hát ' . $data['song']->name . ' MP3 - Download Miễn Phí';
+		Page::$description = $data['song']->name . ' Mp3, tải bài hát ' . $data['song']->name . ' - ' . $data['song']->single . ', tải nhạc chất lượng cao. Miễn phí download về máy. Tải dễ dàng và nhanh chóng ♥';
 
-        // Nếu không có skymusic thì không hiển thị adsense
-        if ( ! $data['song']->hasSkymusic) {
-            Page::$IS_ADSENSE = 0;
-        }
+		// Nếu không có skymusic thì không hiển thị adsense
+		if (!$data['song']->hasSkymusic) {
+			Page::$IS_ADSENSE = 0;
+		}
 
-        // Nếu không có skymusic thì không index
-        if ( ! $data['song']->hasSkymusic && ! $data['song']->canDownload) {
-            Page::$NO_INDEX = true;
-        }
+		// Nếu không có skymusic thì không index
+		if (!$data['song']->hasSkymusic && !$data['song']->canDownload) {
+			Page::$NO_INDEX = true;
+		}
 
-        return view(
-            Core::viewPath('song'),
-            array_merge(['__core' => $core], $data)
-        );
-    }
+		return view(
+			Core::viewPath('song'),
+			array_merge(['__core' => $core], $data)
+		);
+	}
 }
