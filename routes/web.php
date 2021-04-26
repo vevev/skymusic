1845717a62963d7738534c95a7ea89aec093db8c
vevev <taibaihat.mobi@gmail.com>
2021-04-26 20:51:46 +0700

    ok

--- routes/web.php
+++ routes/web.php
@@ -8,39 +8,39 @@ Route::pattern('single_slug', '[a-z0-9\-]+');
 Route::pattern('slug', '[^/\.]*');
 Route::pattern('page', '\d+');
 
-if(preg_match('~BlackPink~i', $_SERVER['REQUEST_URI'])){
+if (isset($_SERVER['REQUEST_URI']) && preg_match('~BlackPink~i', $_SERVER['REQUEST_URI'])) {
     abort(404);
 }
-   
+
 $domain = config('app.domain');
 switch ($domain) {
-case 'skymusic.dev':
-	Route::domain($domain)
-		->group(base_path('routes/domains/trangtainhac.net.php'));
-	// ->group(base_path('routes/domains/tainhac123.com.php'));
-	break;
-case 'skymusic.com':
-	Route::domain($domain)
-		->group(base_path('routes/domains/tainhac123.com.php'));
-	// ->group(base_path('routes/domains/tainhac123.com.php'));
-	break;
-case 'trangtainhacnet.dev':
-	Route::domain($domain)
-		->group(base_path('routes/domains/trangtainhac.net.php'));
-	break;
+    case 'skymusic.dev':
+        Route::domain($domain)
+            ->group(base_path('routes/domains/trangtainhac.net.php'));
+        // ->group(base_path('routes/domains/tainhac123.com.php'));
+        break;
+    case 'skymusic.com':
+        Route::domain($domain)
+            ->group(base_path('routes/domains/tainhac123.com.php'));
+        // ->group(base_path('routes/domains/tainhac123.com.php'));
+        break;
+    case 'trangtainhacnet.dev':
+        Route::domain($domain)
+            ->group(base_path('routes/domains/trangtainhac.net.php'));
+        break;
 
-case 'trangtainhac.net':
-	Route::domain($domain)
-		->group(base_path('routes/domains/trangtainhac.net.php'));
-	break;
+    case 'trangtainhac.net':
+        Route::domain($domain)
+            ->group(base_path('routes/domains/trangtainhac.net.php'));
+        break;
 
-case 'test.tainhac123.com':
-	Route::domain($domain)
-		->group(base_path('routes/domains/trangtainhac.net.php'));
-	break;
+    case 'test.tainhac123.com':
+        Route::domain($domain)
+            ->group(base_path('routes/domains/trangtainhac.net.php'));
+        break;
 
-case 'tainhac123.com':
-	Route::domain($domain)
-		->group(base_path('routes/domains/tainhac123.com.php'));
-	break;
+    case 'tainhac123.com':
+        Route::domain($domain)
+            ->group(base_path('routes/domains/tainhac123.com.php'));
+        break;
 }
