ef161acc154e86dd81677e166ca23f4a18e92d77
vuongnq <taibaihat.mobi@gmail.com>
2021-06-30 20:11:09 +0700

    Xoa sax2PPGbyi00

--- app/Http/Middleware/VerifyIdSong.php
+++ app/Http/Middleware/VerifyIdSong.php
@@ -25,7 +25,7 @@ class VerifyIdSong
         $list = [
             'b2ki2Wga0XWX', '2Lv43QXMeumT', 'lGg0mLsOT3qm', 'aCQIv6EGgdM8', 'wk1C93W74Gnf',
             'wFxBcbOz14GV', 'PKDK5YVEdgWH', 'iOYzPfp6lchL', '5z6HLpEj8AKI', 'BSthd9FKKLm3',
-            'Hyda6ukMOr1t', 'PWAHV0oEsD', 'Twdo2DGj5gtH', 'VbyPgbZRh2gQ',
+            'Hyda6ukMOr1t', 'PWAHV0oEsD', 'Twdo2DGj5gtH', 'VbyPgbZRh2gQ', 'sax2PPGbyi00'
         ];
 
         return in_array($id, $list);
