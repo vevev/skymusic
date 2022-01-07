584b4a7a8e315eabaabd910b526f7a50db0e7181
vdev <taibaihat.mobi@gmail.com>
2022-01-07 08:43:24 +0700

    Update NCTSong.php

--- app/Models/NCTSong.php
+++ app/Models/NCTSong.php
@@ -229,7 +229,7 @@ class NCTSong extends Model
      */
     public function expired()
     {
-        return $this->updated_at->diffInDays(now()) > self::MAXIMUM_STORAGE_DAYS;
+        return false;//$this->updated_at->diffInDays(now()) > self::MAXIMUM_STORAGE_DAYS;
     }
 
     /**
